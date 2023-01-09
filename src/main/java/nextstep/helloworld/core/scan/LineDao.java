13e3d2741630b1aad05f7cb9f520f6064bf7b242
Sangyups <feels4637@gmail.com>
2023-01-09 17:59:59 +0900

    학습 테스트 - Spring Core

--- src/main/java/nextstep/helloworld/core/scan/LineDao.java
+++ src/main/java/nextstep/helloworld/core/scan/LineDao.java
@@ -1,4 +1,11 @@
 package nextstep.helloworld.core.scan;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
 public class LineDao {
+
 }
